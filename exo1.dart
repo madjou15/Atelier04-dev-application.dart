43f045d4322b36a120d1bd366b7aab7efe617bdc
madjou15 <thiernomadjoubah450@gmail.com>
2025-10-13 19:51:57 -0400

    creation methode afficher

--- exo1.dart
+++ exo1.dart
@@ -1,6 +1,13 @@
 class Couleur {
   final int r, g, b;
   Couleur(this.r , this.g, this.b);
+  Couleur.rouge() : r = 255, g = 0, b = 0;
+Couleur.vert() : g = 255, r = 0, b = 0;
+Couleur.bleue() : b = 255, r = 0, g = 0;
+
+void afficherCouleur(){
+  print("r : $r, v : $g, b : $b");
+}
 
 
 
