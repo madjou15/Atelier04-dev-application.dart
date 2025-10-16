d0f074b7b02fe2f51c2706514e92b35a89f674ea
madjou15 <thiernomadjoubah450@gmail.com>
2025-10-16 14:35:13 -0400

    implementation de la methode deconnecter() pour qu'elle affiche :ServeurAPI: Deconnection reussi

--- exo6.dart
+++ exo6.dart
@@ -3,3 +3,16 @@ abstract class Connectable {
   void deconnecter();
 }
 
+// Classe ServeurAPI qui implémente Connectable
+class ServeurAPI implements Connectable {
+  @override
+  void connecter(String utilisateur) {
+    print("ServeurAPI : Connexion établie pour $utilisateur.");
+  }
+
+  @override
+  void deconnecter() {
+    print("ServeurAPI : Déconnexion réussie.");
+  }
+}
+
