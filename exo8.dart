abbb0697dac25e5a011afe50f6f3705171aec11f
madjou15 <thiernomadjoubah450@gmail.com>
2025-10-17 12:46:10 -0400

    ajout de la methode pour afficher le genre

--- exo8.dart
+++ exo8.dart
@@ -24,4 +24,24 @@ class Roman extends Livre {
 
   Roman(String titre, String auteur, this.genre) : super(titre, auteur);
 
-  
+  void afficherRoman() {
+    print("Titre: $titre, Auteur: $auteur, Genre: $genre, Pages: $pages");
+  }
+}
+
+void main() {
+ 
+  var livre1 = Livre("L’enfant noir", "Camara Laye");
+  var livre2 = Livre("Nations nègres et culture", "Cheikh Anta Diop");
+
+  livre1.afficherInfos();
+  livre2.afficherInfos();
+
+  var roman1 = Roman("Les Soleils des indépendances", "Camara Laye", "Roman africain");
+  var roman2 = Roman("Civilisation ou barbarie", "Cheikh Anta Diop", "Essai historique");
+
+  roman1.afficherRoman();
+  roman2.afficherRoman();
+
+  Livre.afficherTotalLivres();
+}
