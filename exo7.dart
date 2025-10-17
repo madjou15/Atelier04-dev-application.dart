c59580cb95441cdbf44d1ac282a08bda8a9295aa
madjou15 <thiernomadjoubah450@gmail.com>
2025-10-17 12:16:45 -0400

    creation des 3 instances dans main()et afficher la valeur finale

--- exo7.dart
+++ exo7.dart
@@ -10,4 +10,14 @@ class Tache {
   }
 }
 
+void main() {
+  var t1 = Tache("Faire les devoirs");
+  var t2 = Tache("Aller à la salle");
+  var t3 = Tache("Lire un livre");
 
+  print(t1.description);
+  print(t2.description);
+  print(t3.description);
+
+  print("Nombre total de tâches créées : ${Tache.nombreTotal}");
+}
